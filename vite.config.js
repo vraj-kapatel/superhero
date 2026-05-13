0c4a7b57af72c1edac08b5d1e2a1d1fd99b8dd56
Vraj Kapatel <kapatel001@gannon.edu>
2026-05-13 02:12:59 -0400

    Add plugins array to vite config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
+  plugins: [],
   base: '/', // Adjust if your app is served from a subpath
   build: {
     rollupOptions: {
